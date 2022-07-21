aff4feeca5395278680ee50f80d899b17abdb575
Corey Raymond DeMarse <1267870+coreydemarse@users.noreply.github.com>
2022-07-21 18:05:15 -0400

    add messages to validation chain

--- src/validations/contact.ts
+++ src/validations/contact.ts
@@ -2,21 +2,21 @@ import { check, ValidationChain } from "express-validator"
 
 export const postSend: ValidationChain[] = [
 	//check existence
-	check("name").not().isEmpty(),
-	check("email").not().isEmpty(),
-	check("message").not().isEmpty(),
+	check("name").not().isEmpty().withMessage("name is required"),
+	check("email").not().isEmpty().withMessage("email is required"),
+	check("message").not().isEmpty().withMessage("message is required"),
 
 	//check types
-	check("name").isString(),
-	check("email").isString(),
-	check("message").isString(),
+	check("name").isString().withMessage("name must be a string"),
+	check("email").isString().withMessage("email must be a string"),
+	check("message").isString().withMessage("message must be a string"),
 
 	// name
-	check("name").isLength({ min: 1, max: 60 }),
+	check("name").isLength({ min: 1, max: 60 }).withMessage("name must be between 1 and 60 characters"),
 
 	// email
-	check("email").isEmail().normalizeEmail(),
+	check("email").isEmail().normalizeEmail().withMessage("email must be a valid email address"),
 
 	// message
-	check("message").isLength({ min: 1, max: 1000 })
+	check("message").isLength({ min: 1, max: 1000 }).withMessage("message must be between 1 and 1000 characters"),
 ]
