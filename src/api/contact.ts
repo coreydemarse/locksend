84528be74da1e7808880c0a2828178ef7fe89451
Corey Raymond DeMarse <1267870+coreydemarse@users.noreply.github.com>
2022-07-21 18:28:17 -0400

    add formatted result to response

--- src/api/contact.ts
+++ src/api/contact.ts
@@ -148,7 +148,7 @@ export default class Contact {
 
 		// catch all validation errors
 		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() })
+			return res.status(400).json({ errors: myValidationResult(req).array() })
 		}
 
 		const name = req.body.name
